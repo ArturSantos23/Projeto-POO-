a99981160f7eae48e787d05cb1daf41f61f15cf1
Alexandre Moreira <65826275+a21966@users.noreply.github.com>
2021-12-13 10:15:35 +0000

    f

--- Main.cpp
+++ Main.cpp
@@ -2,9 +2,9 @@ using namespace std;
 #include <iostream>
 #include <stdlib.h>
 #include <string.h>
-#include "Sistemaficheiros.h"
 #include <dirent.h>
-#include<sys/stat.h>
+#include <sys/stat.h>
+#include "Sistemaficheiros.h"
 
 int main()
 {

6dbfb95e6a39a28b8a434ff8c2b1f27096ad3534
Artur Santos <artur.correia.santos2002@gmail.com>
2022-01-10 19:15:24 +0000

    Final v2

--- Main.cpp
+++ Main.cpp
@@ -5,16 +5,6 @@
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
-
-    /*Teste de memoria
-        while(1)
-        {
-            SistemaFicheiros *SF = new SistemaFicheiros();
-            SF->LoadRoot(current_dir);
-            delete SF;
-        }
-    */
-
     SistemaFicheiros* F = new SistemaFicheiros();
     Menus::Select(F);
 
